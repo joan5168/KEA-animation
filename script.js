95fd6b511492718cd3a15b304560ecb1c360fdb5
Joanna Nguyen <joannanguyen@Joannas-MacBook-Pro.local>
2018-11-02 21:25:57 +0100

    03_02_04-spil med pointgivning

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+let score = 0;
+
 window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("siden vises");
     //Hvad der skal ske
     showStart();
+
 }
 
 
@@ -15,12 +18,12 @@ function showStart() {
     document.querySelector("#play").classList.add("pulse");
     document.querySelector("#start").addEventListener("click", hideStart);
 
-
 }
 
 
 function hideStart() {
     console.log("hide start");
+
 
     //document.querySelector("#start").classList.add("hide");
     document.querySelector("#play").classList.remove("pulse");
@@ -34,8 +37,6 @@ function hideStart() {
 
 
 
-
-
 function startGame() {
     console.log("start game");
 
@@ -45,4 +46,174 @@ function startGame() {
     document.querySelector("#start").classList.add("hide");
 
 
+    document.querySelector("#points").innerHTML = "Score" + score;
+    document.querySelector("#bananskrald").addEventListener("click", bananskraldClickHandler);
+    document.querySelector("#drink").addEventListener("click", drinkClickHandler);
+    document.querySelector("#flaske").addEventListener("click", flaskeClickHandler);
+    document.querySelector("#katon").addEventListener("click", katonkClickHandler);
+    document.querySelector("#papir").addEventListener("click", papirClickHandler);
+    document.querySelector("#plastikpose").addEventListener("click", plastikposeClickHandler);
+    document.querySelector("#sug").addEventListener("click", sugClickHandler);
+    document.querySelector("#vinflaske").addEventListener("click", vinflaskeClickHandler);
+    document.querySelector("#aable").addEventListener("click", aableClickHandler);
+
+
+
 }
+
+
+function bananskraldClickHandler() {
+    console.log("bananskraldClickHandler");
+
+
+    score++;
+    document.querySelector("#points").innerHTML = "Score" + score;
+    console.log(score);
+
+
+
+    console.log(this);
+    this.classList.add("hide");
+
+    this.removeEventListener("click", bananskraldClickHandler);
+
+
+}
+
+//}
+//
+//function drinkClickHandler() {
+//    console.log("-1point");
+//
+//
+//    score--;
+//    console.log(score);
+//
+//    document.querySelector("#points").innerHTML = "Score" + score;
+//
+//    console.log(this);
+//    this.classList.add("hide");
+//
+//    this.removeEventListener("click", drinkClickHandler);
+//
+//
+//}
+//
+//function flaskeClickHandler() {
+//    console.log("1point");
+//
+//
+//    score++;
+//    console.log(score);
+//
+//    document.querySelector("#points").innerHTML = "Score" + score;
+//
+//    console.log(this);
+//    this.classList.add("hide");
+//
+//    this.removeEventListener("click", flaskeClickHandler);
+//
+//
+//}
+//
+//function katonClickHandler() {
+//    console.log("1point");
+//
+//
+//    score++;
+//    console.log(score);
+//
+//    document.querySelector("#points").innerHTML = "Score" + score;
+//
+//    console.log(this);
+//    this.classList.add("hide");
+//
+//    this.removeEventListener("click", katonClickHandler);
+//
+//
+//}
+//
+//function papirClickHandler() {
+//    console.log("1point");
+//
+//
+//    score++;
+//    console.log(score);
+//
+//    document.querySelector("#points").innerHTML = "Score" + score;
+//
+//    console.log(this);
+//    this.classList.add("hide");
+//
+//    this.removeEventListener("click", papirClickHandler);
+//
+//
+//}
+//
+//function plastikposeClickHandler() {
+//    console.log("-1point");
+//
+//
+//    score--;
+//    console.log(score);
+//
+//    document.querySelector("#points").innerHTML = "Score" + score;
+//
+//    console.log(this);
+//    this.classList.add("hide");
+//
+//    this.removeEventListener("click", plastikposeClickHandler);
+//
+//
+//}
+//
+//function sugClickHandler() {
+//    console.log("-1point");
+//
+//
+//    score--;
+//    console.log(score);
+//
+//    document.querySelector("#points").innerHTML = "Score" + score;
+//
+//    console.log(this);
+//    this.classList.add("hide");
+//
+//    this.removeEventListener("click", sugClickHandler);
+//
+//
+//}
+//
+//function vinflaskeClickHandler() {
+//    console.log("1point");
+//
+//
+//    score++;
+//    console.log(score);
+//
+//    document.querySelector("#points").innerHTML = "Score" + score;
+//
+//    console.log(this);
+//    this.classList.add("hide");
+//
+//    this.removeEventListener("click", vinflaskeClickHandler);
+//
+//
+//}
+//
+//function aableClickHandler() {
+//    console.log("1point");
+//
+//
+//    score++;
+//    console.log(score);
+//
+//    document.querySelector("#points").innerHTML = "Score" + score;
+//
+//    console.log(this);
+//    this.classList.add("hide");
+//
+//    this.removeEventListener("click", aableClickHandler);
+//
+//
+//}
